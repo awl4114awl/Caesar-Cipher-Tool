42f270493ba54e5ad019266be9b8583370cb1aa4
Jordan Calvert <jordanryancalvert@gmail.com>
2024-07-22 19:33:23 +0000

    caesar_cipher.py correction

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,7 +5,7 @@ def caesar_cipher(text, shift, encrypt=True):
     for i in range(len(text)):
         char = text[i]
         if char.isalpha():
-            shift_base = 65 if char is.upper() else 97
+            shift_base = 65 if char.isupper() else 97
             result += chr((ord(char) + shift - shift_base) % 26 + shift_base)
         else:
             result += char
